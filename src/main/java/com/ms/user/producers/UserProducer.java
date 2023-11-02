a75a8b5a99ab4a995ac7d31c9fae7ec5412a3d22
Alef <alef.m.pires@gmail.com>
2023-11-02 00:17:29 -0300

    first commit

---
+++ src/main/java/com/ms/user/producers/UserProducer.java
@@ -0,0 +1,32 @@
+package com.ms.user.producers;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.ms.user.dtos.EmailDTO;
+import com.ms.user.models.UserModel;
+
+@Component
+public class UserProducer {
+
+  final RabbitTemplate rabbitTemplate;
+
+  public UserProducer(RabbitTemplate rabbitTemplate) {
+    this.rabbitTemplate = rabbitTemplate;
+  }
+
+  @Value(value = "${broker.queue.email.name}")
+  private String routingKey;
+  
+  public void publishMessageEmail(UserModel userModel) {
+    var emailDTO = new EmailDTO();
+    emailDTO.setUserID(userModel.getUserId());
+    emailDTO.setEmailTo(userModel.getEmail());
+    emailDTO.setSubject("Cadastro realizado com sucesso!");
+    emailDTO.setText(userModel.getName() + ", seja bem vindo(a)! \nAgradecemos o seu cadastro. Aproveite agora todos os recurso da nossa plataforma South System!" );
+
+    rabbitTemplate.convertAndSend("", routingKey, emailDTO);
+  }
+  
+}
